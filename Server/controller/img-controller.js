9e59d72499441ee364cccabc7d9b5655ee9de94f
Itsarun01 <hey.arunkumar05@gmail.com>
2024-11-22 22:19:02 +0530

    mk

--- Server/controller/img-controller.js
+++ Server/controller/img-controller.js
@@ -1,6 +1,5 @@
 export const uploadImg = (req, res) => {
   try {
-    console.log(req);
   } catch (error) {
     console.log("Error while uploading image", error.message);
   }
--- Server/database/db.js
+++ Server/database/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const dbConnection = async () => {
-  const DB_URL = "mongodb://localhost:27017/";
+  const DB_URL =
+    "mongodb+srv://user01:1234@file-sharing-app.t5vq0.mongodb.net/?retryWrites=true&w=majority&appName=file-sharing-app";
   try {
     await mongoose.connect(DB_URL, {
       useNewUrlParser: true,
